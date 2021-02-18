2fdfe323f86a3ff1dfe1893059f9720953963081
kerncl <kerncl@hotmail.com>
2021-02-18 18:08:59 +0800

    added stock webpage

--- src/web/frontend/routes.py
+++ src/web/frontend/routes.py
@@ -9,6 +9,7 @@ from flask import render_template, request
 from src.web.backend.forms import StockForm
 from src.build_data.Conversion import GenerateDB
 from src.database.table import Company, News
+from src.build_data.generateData import web_scrapping_news
 
 
 template_folder = pathlib.Path(__file__).parent.parent.joinpath('templates').__str__()
@@ -27,13 +28,13 @@ def index():
 @app.route('/home', methods=['POST', 'GET'])
 def search():
     stockform = StockForm(request.form)
-    if request.method == 'POST':    # when submit has been clicked
+    if request.method == 'POST':  # when submit has been clicked
         print('* Accessing POST Method')
         print(f'User enter:\nCompany: {stockform.company.data}\n '
               f'Company full name: {stockform.company_fullname.data}\n'
               f'Category: {stockform.category.data}\n'
-              f'Market: {stockform.market.data}') # return data
-        company = '%' + stockform.company.data + '%'    # todo: can be improve by using request.form generate with setattr()
+              f'Market: {stockform.market.data}')  # return data
+        company = '%' + stockform.company.data + '%'  # todo: can be improve by using request.form generate with setattr()
         company_full_name = '%' + stockform.company_fullname.data + '%'
         category = stockform.category.data
         market = stockform.market.data
@@ -45,14 +46,23 @@ def search():
                                                        Company.category.like(category),
                                                        Company.market.like(market)).all()
         return render_template('searchresult.html', form=stockform, result=stock_list)
-    elif request.method == 'GET':   # when loaded page
+    elif request.method == 'GET':  # when loaded page
         print('* Accessing GET Method')
         return render_template('home.html', form=stockform)
 
 
 @app.route('/stock/<code>', methods=['GET'])
-def stock(code):    # code obtain from url_for **kwargs
-    return f'<h>Stock Page: {code}</h>'
+def stock(code):  # code obtain from url_for **kwargs
+    with GenerateDB() as session:
+        news_data = session.query(News).filter(News.code == code).one_or_none()
+        print(news_data)
+    news = web_scrapping_news(code)
+    for platform, news_list in news.items():
+        print(platform)
+        print(news_list)
+    pass
+    return render_template('stock.html', result=news)
+    # return f'<h>Stock Page: {code}</h>'
 
 
 @app.teardown_appcontext
@@ -64,5 +74,3 @@ def loaded(exception=None):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
