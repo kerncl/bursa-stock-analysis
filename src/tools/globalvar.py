7001f98c5db6d21a3c75a6f0bd55a880871bb15f
kerncl <kerncl@hotmail.com>
2021-03-29 21:56:37 +0800

    auto generate CSV and DB file if not exists

--- src/tools/globalvar.py
+++ src/tools/globalvar.py
@@ -1,13 +1,19 @@
 import os
 import pathlib
-
-# PATH  # todo: script to verify path, subprocess to handle if no file found ?
-CHROME_EXECUTED_PATH = os.path.abspath('../../external file/chromedriver.exe')
-CSV_PATH = str(pathlib.Path.home().parent.parent.joinpath('temp/stock_list.csv'))
-DB_PATH = os.path.abspath('../database/stock.db')
+from src.build_data.Conversion import GenerateDB
 
 # URL
 KLSE_URL = 'https://www.klsescreener.com'
 I3INVESTOR_URL = 'https://klse.i3investor.com/'
 KLSE_CHART_URL = 'https://www.klsescreener.com/v2/charting/chart/'
 TRADINGVIEW_CHART_URL = "https://www.tradingview.com/chart/?symbol=MYX%3A"
+
+# PATH  # todo: script to verify path, subprocess to handle if no file found ?
+CHROME_EXECUTED_PATH = os.path.abspath('../../external file/chromedriver.exe')
+CSV_PATH = str(pathlib.Path.home().parent.parent.joinpath('temp/stock_list.csv'))
+DB_PATH = os.path.abspath('../database/stock.db')
+
+if not os.path.exists(CSV_PATH):
+    GenerateDB.update_csv()
+    GenerateDB.renew_table(CSV_PATH)
+    print(f'Done generate CSV & DB')
